cfc53b0ea31a0a584556309754d3ce566c0d8e49
Develaupair <develaupair@protonmail.com>
2022-04-23 15:05:23 +0200

    cleanup, added TODOs

--- module_fractions.py
+++ module_fractions.py
@@ -14,7 +14,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import math
 
-"""
+module_description = """
 Class Description:
 This class implements simple fractions, based on python's int data type. 
 At all times, both the nominator and denominator should be of type int to the outside world.
@@ -22,36 +22,53 @@ Floating point numbers and other fractions are not supported internally. However
 by providing a float type nominator to the constructor. The denominator has to be an int type number.
 The following methods are implemented:
 
-(EXTERNAL METHODS)
-Fraction(nominator*, denominator*) -> constructor, creates Fraction based on given number(s)
+(OUTGOING METHODS)
+Fraction(nominator, denominator) -> constructor, creates Fraction based on given number(s)
 - nominator: optional int or float, default is 0
 - denominator: optional int, default is 1, cannot be equal 0
 getn() -> returns current nominator
 getd() -> return current denominator
 setn(nominator) -> sets nominator to new int type number
-setn(denominator) -> sets denominator to new int type number
+setd(denominator) -> sets denominator to new int type number
 print() -> prints a visual representation of the fraction
-getApproximation() -> returns a float type numerical approximation of the fraction (ATTENTION: IMPRECISE!) 
+
+(TYPE CONVERSION METHODS)
+__int__() -> returns int based on the numerical value of the fraction
+__str__() -> returns string representation of fraction, also used for print() object method
+__float__() -> returns a float type numerical approximation of the fraction (ATTENTION: PRECISION LOSS!)
 
 (ARITHMETIC OPERATORS)
 __add__(other) -> implements addition, other can be int, float, or Fraction
-add(other) -> wrapper for __add__(other) object method
 __sub__(other) -> implements subtraction, other can be int, float, or Fraction
-sub(other) -> wrapper for __sub__(other) object method
 __mul__(other) -> implements multiplication, other can be int, float, or Fraction
-mul(other) -> wrapper for __mul__(other) object method
 __truediv__(other) -> implements division, other can be int, float, or Fraction
+(WRAPPER)
+add(other) -> wrapper for __add__(other) object method
+sub(other) -> wrapper for __sub__(other) object method
+mul(other) -> wrapper for __mul__(other) object method
 div(other) -> wrapper for __truediv__(other) object method
 
 (COMPARISON OPERATORS)
-__eq__(other) -> checks for equality based on the string representation
+__lt__(other) -> TODO: COMING SOON
+__le__(other) -> TODO: COMING SOON
+__eq__(other) -> checks for equality, other can be int, float, or Fraction
+__ne__(other) -> checks for inequality, other can be int, float, or Fraction
+__ge__(other) -> TODO: COMING SOON
+__gt__(other) -> TODO: COMING SOON
+(WRAPPER)
 equals(other) -> wrapper for __eq__(other) object method
+notequals(other) -> wrapper for __ne__(other) object method
+
+(UNARY OPERATORS)
+__pos__() # TODO: COMING SOON
+__neg__() # TODO: COMING SOON
+__abs__() # TODO: COMING SOON
+__round__(n) # TODO: COMING SOON
 
 (INTERNAL METHODS)
 deflate() -> shortens the fraction so that equality can be determined correctly
 internalMul(integer) -> multiplies both the nominator and the denominator with a given int type number
-visualInflation() -> inflates floating point nominator in fraction to an int type without loss of precision
-__str__() -> returns visual representation of fraction, also used for print() object method
+visualInflation() -> inflates fraction to next int type without loss of floating point precision
 """
 
 
@@ -59,6 +76,7 @@ class Fraction:
     n = 0  # nominator
     d = 1  # denominator
 
+    # OUTGOING
     def __init__(self, nominator=0, denominator=1):
         assert type(nominator) == int or type(nominator) == float
         assert type(denominator) == int
@@ -92,6 +110,97 @@ class Fraction:
         self.d = denominator
         self.deflate()
 
+    def print(self):
+        print(self)
+
+    # TYPE CONVERSION
+    def __int__(self):
+        return int(self.n / self.d)
+
+    def __str__(self):
+        self.deflate()
+        return f"({self.n}/{self.d})"
+
+    def __float__(self):
+        return float(self.n / self.d)
+
+    # ARITHMETIC
+    def __add__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        result = Fraction(1)
+        a = self
+        b = other
+        m = self.d * other.d
+        result.n = (a.n * b.d) + (b.n * a.d)
+        result.d = m
+        result.deflate()
+        return result
+
+    def __sub__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        result = Fraction(1)
+        a = self
+        b = other
+        m = self.d * other.d
+        result.n = (a.n * b.d) - (b.n * a.d)
+        result.d = m
+        result.deflate()
+        return result
+
+    def __mul__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        result = Fraction()
+        result.setn(self.n * other.n)
+        result.setd(self.d * other.d)
+        result.deflate()
+        return result
+
+    def __truediv__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        tempn = other.getd()
+        tempd = other.getn()
+        tempf = Fraction(tempn, tempd)
+        return self * tempf
+
+    def add(self, fraction):
+        return self + fraction
+
+    def sub(self, fraction):
+        return self - fraction
+
+    def mul(self, other):
+        return self * other
+
+    def div(self, other):
+        return self / other
+
+    # COMPARISON
+    def __eq__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        return self.getn() == other.getn() and self.getd() == other.getd()
+
+    def __ne__(self, other):
+        return not self == other
+
+    def equals(self, other):
+        return self == other
+
+    def notequals(self, other):
+        return self != other
+
+    # UNARY
+
+    # INTERNAL
     def deflate(self):
         greatestCommonDivisor = math.gcd(self.n, self.d)
         while greatestCommonDivisor != 1:
@@ -113,79 +222,3 @@ class Fraction:
             n = len(str(self.n).split(".")[1])
             self.n = int(str(self.n).replace(".", ""))
             self.d = int(self.d * pow(10, n))
-
-    def __str__(self):
-        self.deflate()
-        return f"({self.n}/{self.d})"
-
-    def print(self):
-        print(self)
-
-    def getApproximation(self):
-        return float(self.n / self.d)
-
-    def __add__(self, other):
-        if type(other) != Fraction:
-            other = Fraction(other)
-        assert type(other) == Fraction
-        result = Fraction(1)
-        a = self
-        b = other
-        m = self.d * other.d
-        result.n = (a.n * b.d) + (b.n * a.d)
-        result.d = m
-        result.deflate()
-        return result
-
-    def add(self, fraction):
-        return self + fraction
-
-    def __sub__(self, other):
-        if type(other) != Fraction:
-            other = Fraction(other)
-        assert type(other) == Fraction
-        result = Fraction(1)
-        a = self
-        b = other
-        m = self.d * other.d
-        result.n = (a.n * b.d) - (b.n * a.d)
-        result.d = m
-        result.deflate()
-        return result
-
-    def sub(self, fraction):
-        return self - fraction
-
-    def __mul__(self, other):
-        if type(other) != Fraction:
-            other = Fraction(other)
-        assert type(other) == Fraction
-        result = Fraction()
-        result.setn((self.getn() * other.getn()))
-        result.setd((self.getd() * other.getd()))
-        result.deflate()
-        return result
-
-    def mul(self, other):
-        return self * other
-
-    def __truediv__(self, other):
-        if type(other) != Fraction:
-            other = Fraction(other)
-        assert type(other) == Fraction
-        tempn = other.getd()
-        tempd = other.getn()
-        tempf = Fraction(tempn, tempd)
-        return self * tempf
-
-    def div(self, other):
-        return self / other
-
-    def __eq__(self, other):
-        if type(other) != Fraction:
-            other = Fraction(other)
-        assert type(other) == Fraction
-        return self.getn() == other.getn() and self.getd() == other.getd()
-
-    def equals(self, other):
-        return self == other
