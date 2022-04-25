86cce57aa02f730c14661294381ef51c20153e89
Anubosiris <76901115+Develaupair@users.noreply.github.com>
2022-04-25 23:03:23 +0200

    can somebody please tell me why the repo breaks...
    - implemented more comparison operators
    - added wrapper methods for new comparison operators
    - implemented pos(), neg(), and abs()
    - refactored math.gcd() usage into class method
    - updated module description string

--- module_fractions.py
+++ module_fractions.py
@@ -49,23 +49,28 @@ mul(other) -> wrapper for __mul__(other) object method
 div(other) -> wrapper for __truediv__(other) object method
 
 (COMPARISON OPERATORS)
-__lt__(other) -> TODO: COMING SOON
-__le__(other) -> TODO: COMING SOON
+__lt__(other) -> checks if numerically less, other can be int, float, or Fraction
+__le__(other) -> checks if numerically less or equal, other can be int, float, or Fraction
 __eq__(other) -> checks for equality, other can be int, float, or Fraction
 __ne__(other) -> checks for inequality, other can be int, float, or Fraction
-__ge__(other) -> TODO: COMING SOON
-__gt__(other) -> TODO: COMING SOON
+__ge__(other) -> checks if numerically greater, other can be int, float, or Fraction
+__gt__(other) -> checks if numerically greater or equal, other can be int, float, or Fraction
 (WRAPPER)
 equals(other) -> wrapper for __eq__(other) object method
 notequals(other) -> wrapper for __ne__(other) object method
+greater(other) -> wrapper for __gt__(other) object method
+greaterorequal(other) -> wrapper for __ge__(other) object method
+less(other) -> wrapper for __lt__(other) object method
+lessorequal(other) -> wrapper for __le__(other) object method
 
 (UNARY OPERATORS)
-__pos__() # TODO: COMING SOON
-__neg__() # TODO: COMING SOON
-__abs__() # TODO: COMING SOON
+__pos__() # returns Fraction
+__neg__() # returns negative Fraction
+__abs__() # returns absolute version of Fraction
 __round__(n) # TODO: COMING SOON
 
 (INTERNAL METHODS)
+gcd(firstint, secondint) -> static method for used implementation to get the greatest common divisor of 2 ints
 deflate() -> shortens the fraction so that equality can be determined correctly
 internalMul(integer) -> multiplies both the nominator and the denominator with a given int type number
 visualInflation() -> inflates fraction to next int type without loss of floating point precision
@@ -192,21 +197,74 @@ class Fraction:
     def __ne__(self, other):
         return not self == other
 
+    def __gt__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        a = self.n * other.d
+        b = other.n * self.d
+        return a > b
+
+    def __lt__(self, other):
+        if type(other) != Fraction:
+            other = Fraction(other)
+        assert type(other) == Fraction
+        a = self.n * other.d
+        b = other.n * self.d
+        return a < b
+
+    def __le__(self, other):
+        return not self > other
+
+    def __ge__(self, other):
+        return not self < other
+
     def equals(self, other):
         return self == other
 
     def notequals(self, other):
         return self != other
 
+    def greater(self, other):
+        return self > other
+
+    def greaterorequal(self, other):
+        return self >= other
+
+    def less(self, other):
+        return self < other
+
+    def lessorequal(self, other):
+        return self <= other
+
     # UNARY
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        n = Fraction(1)
+        n.n = -self.n
+        n.d = self.d
+        n.deflate()
+        return n
+
+    def __abs__(self):
+        self.deflate()
+        if self.n < 0:
+            return -self
+        return self
 
     # INTERNAL
+    @staticmethod
+    def gcd(firstint, secondint):
+        return math.gcd(firstint, secondint)
+
     def deflate(self):
-        greatestCommonDivisor = math.gcd(self.n, self.d)
+        greatestCommonDivisor = self.gcd(self.n, self.d)
         while greatestCommonDivisor != 1:
             self.n = self.n // greatestCommonDivisor
             self.d = self.d // greatestCommonDivisor
-            greatestCommonDivisor = math.gcd(self.n, self.d)
+            greatestCommonDivisor = self.gcd(self.n, self.d)
         if self.d < 0:
             self.internalMul(-1)
         return
